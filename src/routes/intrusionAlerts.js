7ee428cce38366eeab4e397286a0226f26506d4b
Nishant Iyer <88764215+NishantIyer@users.noreply.github.com>
2023-07-23 17:10:53 +0530

    Update intrusionAlerts.js

--- src/routes/intrusionAlerts.js
+++ src/routes/intrusionAlerts.js
@@ -1,10 +1,11 @@
-// src/routes/intrusionAlerts.js
-
 const express = require('express');
 const router = express.Router();
 const intrusionAlertsController = require('../controllers/intrusionAlerts');
 
 // Route to store intrusion alerts
 router.post('/', intrusionAlertsController.storeIntrusionAlert);
+
+// Route to fetch all intrusion alerts
+router.get('/', intrusionAlertsController.getAllIntrusionAlerts);
 
 module.exports = router;
