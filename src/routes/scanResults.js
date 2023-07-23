de50f19f4ab879ed91ed7c544430d9554f3382ea
Nishant Iyer <88764215+NishantIyer@users.noreply.github.com>
2023-07-23 13:06:26 +0530

    Update scanResults.js

--- src/routes/scanResults.js
+++ src/routes/scanResults.js
@@ -1,13 +1,38 @@
-// src/routes/scanResults.js
+// controllers/scanResultsController.js
 
-const express = require('express');
-const router = express.Router();
-const scanResultsController = require('../controllers/scanResultsController');
+const ScanResult = require('../models/scanResult');
 
-// Route to store scan results
-router.post('/', scanResultsController.storeScanResults);
+// Function to store scan results
+const storeScanResults = async (req, res) => {
+  try {
+    const scanResultData = req.body; // Assuming the POST request sends the scan result data as a JSON object
 
-// Route to fetch all scan results
-router.get('/', scanResultsController.getAllScanResults);
+    // Delete all previous scan results before saving the new one
+    await ScanResult.deleteMany({});
 
-module.exports = router;
+    const scanResult = new ScanResult(scanResultData);
+    await scanResult.save();
+
+    // Fetch all scan results after the new one is saved
+    const allScanResults = await ScanResult.find();
+
+    res.status(200).json({ message: 'Scan results received and updated successfully', allScanResults });
+  } catch (error) {
+    res.status(500).json({ error: 'Failed to save scan results' });
+  }
+};
+
+// Function to fetch all scan results
+const getAllScanResults = async (req, res) => {
+  try {
+    const allScanResults = await ScanResult.find();
+    res.status(200).json(allScanResults);
+  } catch (error) {
+    res.status(500).json({ error: 'Failed to fetch scan results' });
+  }
+};
+
+module.exports = {
+  storeScanResults,
+  getAllScanResults,
+};
