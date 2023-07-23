d554202cd7a8c29cc80b78fed47b6f578365133b
Nishant Iyer <88764215+NishantIyer@users.noreply.github.com>
2023-07-23 11:53:31 +0530

    Update db.js

--- src/config/db.js
+++ src/config/db.js
@@ -1,3 +1,3 @@
 // config/db.js
 
-module.exports = 'mongodb://atlas-sql-64ba5e7f8732001dcfd92f2e-711v6.a.query.mongodb.net/myVirtualDatabase?ssl=true&authSource=admin';
+module.exports = 'mongodb+srv://offsecnish2609:idk@cluster0.wwxsx22.mongodb.net/?retryWrites=true&w=majority';
