226702122a7f700018ba7ef11bcb5ebef93c221b
Nishant Iyer <88764215+NishantIyer@users.noreply.github.com>
2023-08-14 14:05:04 +0530

    Update db.js

--- src/config/db.js
+++ src/config/db.js
@@ -1,5 +1,5 @@
 // config/db.js
 
-module.exports = 'mongodb+srv://offsecnish2609:offmongo@cluster0.wwxsx22.mongodb.net/?retryWrites=true&w=majority';
+module.exports = 'your mongo db url';
 
 //Doesn't work, completely broken
